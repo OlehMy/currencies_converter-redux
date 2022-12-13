309a92fcafaab33fcbd50f5bc9f0ea1b2f3148ef
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-12-13 20:42:20 +0200

    night theme colors changed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
+import classNames from 'classnames';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Store } from '../../types';
 import { NavBar } from '../NavBar';
 import classes from './Header.module.scss';
 
 export const Header: React.FC = () => {
+  const { themeChecked } = useSelector((store: Store) => store.currencies);
+
   return (
-    <header className={classes.header}>
+    <header
+      className={classNames(classes.header, {
+        [classes.header__nightTheme]: themeChecked,
+      })}
+    >
       <div className={classes.header__wrapper}>
         <NavBar />
       </div>
